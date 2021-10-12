c9a85397b42eae798e0e6bbb6ed90b0c112ca9bb
Mikhail Trofimov <easymeowus@gmail.com>
2021-10-13 00:09:23 +0400

    Initial commit

---
+++ src/main/java/com/example/differential_equations/DifferentialEquationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.differential_equations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DifferentialEquationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DifferentialEquationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/differential_equations/DifferentialEquationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.differential_equations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DifferentialEquationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
